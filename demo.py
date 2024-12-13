0c91fc6b007784338f699d452af2f89b323468c8
tthelen <tt@tobiasthelen.de>
2024-12-13 13:01:49 +0100

    included url_for

--- demo.py
+++ demo.py
@@ -2,15 +2,14 @@
 from flask import Flask, render_template
 from flask import send_from_directory
 
-
 app = Flask(__name__)
 
-
 @app.route('/assets/<path:path>')
-def send_report(path):
+def send_asset(path):
     # Using request args for path will expose you to directory traversal attacks
     print("The path is: ", path)
     return send_from_directory('assets', path)
+
 
 @app.route('/')
 def hello_world():
